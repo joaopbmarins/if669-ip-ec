3d342ba8f74002e2db11a279e4da47d1a2846dd3
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2023-06-18 04:11:34 -0300

    resolvido o f1>f2

--- lista_2_lacos/lulupalooza/lulupalooza.c
+++ lista_2_lacos/lulupalooza/lulupalooza.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int num_recebido, n_canais, f1, f2;
+    int num_recebido, n_canais, f1=0, f2=0, ftemp=0;
     scanf("%d %d", &num_recebido, &n_canais);
 
     for(int i=0; i<n_canais; i++){//laço para cada canal 
         scanf("%d %d", &f1 ,&f2);
-        while(! (num_recebido < f2 && num_recebido > f1)){
-            f1 = f1 + f2;
+        while(! (num_recebido <= f2 && num_recebido >= f1)){//laço para checar se o número está entre o quais f1 e f2
+            printf("%d %d %d\n", f1, f2, ftemp);
+            ftemp = f1 + f2;
+            f1 = f2;
+            f2 = ftemp;
         }
+        
+        printf("%d %d", f1, f2);
     }
     
 
