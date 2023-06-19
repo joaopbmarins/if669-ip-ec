4e1c9dc3ef8f75576e3b9f3fbe4816fbf5a1815d
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2023-06-18 23:22:38 -0300

    só falta um caso

--- lista_2_lacos/lulupalooza/lulupalooza.c
+++ lista_2_lacos/lulupalooza/lulupalooza.c
@@ -1,21 +1,69 @@
 #include <stdio.h>
 
 int main(){
-    int num_recebido, n_canais, f1=0, f2=0, ftemp=0;
+    int num_recebido, n_canais, f1=0, f2=0, ftemp=0, f=2,fr, canal, dist_fr=0, soma=0, num_soma;
     scanf("%d %d", &num_recebido, &n_canais);
 
-    for(int i=0; i<n_canais; i++){//laço para cada canal 
+    for(int i=1; i<=n_canais; i++){//laço para cada canal   i = canal
         scanf("%d %d", &f1 ,&f2);
+        f = 2;
         while(! (num_recebido <= f2 && num_recebido >= f1)){//laço para checar se o número está entre o quais f1 e f2
-            ftemp = f1 + f2;
+            f++;//incrementa f para saber em qual f2 foi achado o espaço
+            ftemp = f1 + f2;//ajeita a ordem dos fs
             f1 = f2;
             f2 = ftemp;
         }
-        
-        printf("%d %d", f1, f2);
+        if((f2 - num_recebido) > (num_recebido - f1)){ //distancia do f1 pro num/ menor
+            if((num_recebido - f1)<=dist_fr && i!=1){
+                dist_fr = (num_recebido - f1);//define dist_fr como a menor distancia entre f1 e f2 para o num
+                canal = i;
+                fr = f-1;
+                num_soma = f1;
+            }
+            else if(i==1){
+                dist_fr = (num_recebido - f1);
+                canal = i;
+                num_soma = f1;
+                fr = f-1;}
+        }
+        else if((f2 - num_recebido) < (num_recebido - f1)){//distancia do f2 pro num/ menor
+            if((f2 - num_recebido)<=dist_fr && i!=1){
+                dist_fr = f2 - num_recebido;//define dist_fr como a menor distancia entre f1 e f2 para o num
+                canal = i;
+                num_soma = f2;
+                fr = f;
+            }
+            else if(i==1){
+                dist_fr = f2 - num_recebido;
+                canal = i;
+                num_soma = f2;
+                fr = f;}
+        }
+        else if((f2 - num_recebido) == (num_recebido - f1)){
+            dist_fr = (num_recebido - f1);//define dist_fr como a menor distancia entre f1 e f2 para o num
+            canal = i;
+            num_soma = f1;
+            fr = f-1;
+        }
+        //printf("\n%d %d\n",fr ,dist_fr);
     }
-    
+    //printf("\ncanal: %d minuto: %d\n", canal, fr);
 
+    while(num_soma>0){
+        int algoritmo;
+        algoritmo = num_soma % 10;
+        soma += algoritmo;
+        num_soma = num_soma/10;
+        
+    }
+    //printf("%d", soma);
+
+    if(soma>10 && n_canais >0){
+        printf("Xupenio, para ir ao lulupalooza vc deve entrar no canal %d e sera chamado mais ou menos no minuto %d e com o VIP garantido!!!\n", canal, fr);
+    }
+    else if(soma<=10 && soma>0 && n_canais >0){
+        printf("Xupenio, para ir ao lulupalooza vc deve entrar no canal %d e sera chamado mais ou menos no minuto %d, mas o ingresso VIP não vai rolar :(\n", canal, fr);
+    }
 
     return 0;
 }
