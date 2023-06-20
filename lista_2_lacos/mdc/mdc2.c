133a200920ca992cc5958a72b03d9602b8c708f2
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2023-06-20 09:28:54 -0300

    só falta corrigir a logica pro div_min

--- lista_2_lacos/mdc/mdc2.c
+++ lista_2_lacos/mdc/mdc2.c
@@ -1,64 +1,76 @@
 #include <stdio.h>
 
 int main(){
-    int x,y;
-    int resto;
+    long int x,y;
+    long int resto;
 
-    int a=0, b=0;
-    int divisores=0, max=0, min=0, primo = 0, div_max = 0, div_min=0;
+    long int a=0, b=0;
+    long int divisores=0, max=0, min=0, primo = 0, div_max = 0, div_min=0;
 
-    scanf("%d %d", &x, &y);
+    scanf("%ld %ld", &x, &y);
 
     for(int num = x; num<=y; num++){
         divisores = 0;
-        for(int k = 2; k<=num;k++){
-            if(k == 2)
+            if(num == 2)
                 primo = 1;
-            for(int j = 2; j<k; j++){
-                if(k == 1)
+            for(int j = 2; j<num; j++){
+                if(num == 1)
                     primo = 1;
-                else if(k == 2)
+                else if(num == 2)
                     primo = 1;
-                else if(k % j == 0)
+                else if(num % j == 0)
                     primo = 0;
                 else
                     primo = 1;}
-            if(num % k == 0 && primo == 1){
-                divisores++;
-                }
-    }
+            for(int k=2; k<num;k++){         
+                if(num % k == 0 && primo == 1){
+                    divisores++;
+                }}
+    
     //printf("%d-%d\n", x, num);
+    if(num==4)
+        printf("%d-%d\n", num, divisores);
 
-    if(num == x && divisores != 1){
+
+    if(divisores==0)
+        continue;
+    else if(num == x && divisores != 1){
         div_max = divisores;
         max = num;}
     else if(div_max == 0 && divisores >1){
         max = num;
         div_max = divisores;}
+    else if(div_max == divisores && max<num){
+        continue;
+    }
     else if(divisores>=div_max){
         min = max;
         max = num;
         div_min = div_max;
         div_max = divisores;}
-    else if(divisores<=div_max){
-        if(divisores<=div_min){
+    else if(divisores<div_max){
+        if(divisores<div_min){
             min = num;
             div_min = divisores;}
         }
-
+        else if(divisores<= div_min && min <= num){
+            div_min = divisores;
+            min = num;
+    }
     //printf("%d %d\n", max, min);
     }
 
 
+    long int mdc = max;
+    long int mdc2 = min;
 
-
-    while (y != 0) {
-        resto = x % y;
-        x = y;
-        y = resto;
+    while (mdc2 != 0) {
+        resto = mdc % mdc2;
+        mdc = mdc2;
+        mdc2 = resto;
     }
     
-    printf("%d %d %d\n", max, min, x);
+    printf("%ld %ld %ld\n", max, min, mdc);
 
     return 0;
 }
