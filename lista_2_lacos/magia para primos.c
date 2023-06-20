152ce27d0fc528d54045592ac5c687976cafbe8e
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2023-06-20 17:51:08 -0300

    magia virou dudinha kkkkkkkkk

--- lista_2_lacos/magia para primos.c
+++ lista_2_lacos/magia para primos.c
@@ -1,42 +1,55 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int j,k, num = 4,num_o = num, primo = 0 ,divisores = 0;
-    
+    long int n, k_q;
+    int j,k, num = 0, primo = 0 ,p_calculado = 0, p_divisores = 0;
+    int p, q=0;
+
+    scanf("%ld %ld %d", &n,&k_q,&p);
     //antes_for:
-    for(k = 2; k<=num;k++){
-        printf("%d ", k);
-        if(k == 2)
-            primo = 1;
-        for(j = 2; j<k; j++){
-            if(k == 1)
+    for(int i = n; i<k_q;i++){
+        num = i;
+        
+        while(num>1 && p_divisores != -1){
+        p_calculado = 0;
+        p_divisores = 0;
+        for(k = 2; k<=num;k++){
+            if(k == 2)
                 primo = 1;
-            else if(k == 2)
-                primo = 1;
-            else if(k % j == 0)
-                primo = 0;
-            else
-                primo = 1;}
-        if(num % k == 0 && primo == 1){
-            //num = num/k;
-            divisores++;
-            //goto antes_for;
+            for(j = 2; j<k; j++){
+                if(k == 1)
+                    primo = 1;
+                else if(k == 2)
+                    primo = 1;
+                else if(k % j == 0)
+                    primo = 0;
+                else
+                    primo = 1;}
+            if(num % k == 0 && primo == 1){
+                num = num/k;
+                if(k != 2){
+                    if(p_divisores != k){
+                        p_divisores = k;
+                        p_calculado++;
+                    }
+                    else{
+                        p_divisores = -1;
+                    }
+                }
+                    
+                //goto antes_for;
                 }
     }
+    }
+    if(p_calculado == p && p_divisores != -1){
+        q++;
+        printf("%d=%d\n",p_divisores, i); 
+    }
+    }
     
-    if(num == num_o)
-        printf("%d não tem divisores",divisores);
-    else
-        printf("-%d-%d",num, divisores);
+    printf("%d\n",q);
+    
         
         
     return 0;
