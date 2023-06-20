770f05ea8eebb62213143e4a2fd717ac46b1106a
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2023-06-20 16:56:57 -0300

    mdc final

--- lista_2_lacos/mdc/mdc2.c
+++ lista_2_lacos/mdc/mdc2.c
@@ -26,37 +26,27 @@ int main(){
                 if(num % k == 0 && primo == 1){
                     divisores++;
                 }}
-    
-    //printf("%d-%d\n", x, num);
-    if(num==4)
-        printf("%d-%d\n", num, divisores);
 
 
-    if(divisores==0)
-        continue;
-    else if(num == x && divisores != 1){
+    if(divisores==0);
+    else if(div_max == 0 && div_min == 0 && divisores>=1){
+        min = num;
+        max = num;
+        div_min = divisores;
         div_max = divisores;
-        max = num;}
-    else if(div_max == 0 && divisores >1){
-        max = num;
-        div_max = divisores;}
-    else if(div_max == divisores && max<num){
-        continue;
     }
-    else if(divisores>=div_max){
-        min = max;
-        max = num;
-        div_min = div_max;
-        div_max = divisores;}
-    else if(divisores<div_max){
-        if(divisores<div_min){
-            min = num;
-            div_min = divisores;}
-        }
-        else if(divisores<= div_min && min <= num){
+    else if(divisores > div_max && div_max !=0){
+        if(max<=num){
+            div_max = divisores;
+            max = num;}
+    }
+    else if(divisores < div_min && div_min !=0){
+        if(min<=num){
             div_min = divisores;
-            min = num;
+            min = num;}
     }
+
+    
     //printf("%d %d\n", max, min);
     }
 
