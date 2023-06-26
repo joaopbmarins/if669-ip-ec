d64ab929e54f9504b83c96e31ccf61873f121c97
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2023-06-26 09:57:56 -0300

    Update multiplicadora.c

--- lista3/multiplicadora.c
+++ lista3/multiplicadora.c
@@ -5,7 +5,9 @@ int main(){
     scanf("%d %d %d %d", &m1,&n1,&m2,&n2);
     int matriz1[m1][n1];
     int matriz2[m2][n2];
-    int matriz3[m1][n2];
+    int m3 = m1,n3 = n2;
+    int matriz3[m3][n3];
+    
 
     for(int i=0;i<m1;i++){
         for(int j=0;j<n1;j++)
@@ -17,19 +19,25 @@ int main(){
             scanf("%d", &matriz2[i][j]);
     }
 
-    for(int i=0;i<m1;i++){
-        for(int j=0;j<n2;j++){
-            for(int k=1;k<m1;k++){
-                if(k==1)
-                    matriz3[i][j] = matriz1[i][j]*matriz2[i][j];
-                else
-                    matriz3[i][j] += matriz1[i][j+k]*matriz2[i+k][j];
+    for(int i=0;i<m3;i++){
+        for(int j=0;j<n3;j++){
+            //int k = 0;
+            int soma1 = 1, soma2 =1;
+            for(int k1=0;k1<m3;k1++){
+                soma1 *= matriz1[i][j+k1];
             }
+            for(int k2=0;k2<n3;k2++){
+                soma2 *= matriz2[i+k2][j];
+            }
+            int soma = soma1 + soma2;
+            matriz3[i][j] = soma;
+            
+            
         }
     }
 
-    for(int i=0;i<m1;i++){
-        for(int j=0;j<n2;j++)
+    for(int i=0;i<m3;i++){
+        for(int j=0;j<n3;j++)
             printf("%d ", matriz3[i][j]);
         printf("\n");
     }
