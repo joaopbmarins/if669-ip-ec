5d3d961da72cd45c1b1d5414687b282ccd372efa
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2023-08-03 11:26:44 -0300

    Update miniteste2.c

--- mp/miniteste2.c
+++ mp/miniteste2.c
@@ -12,18 +12,38 @@ typedef struct{
     Coluna *p;
 }Linha;
 
-int encontraLinha(int linha, Linha **lista, int tam_lista){
+void printMatriz(Linha *matriz, int qtd_linhas);
+int encontraLinha(int linha, Linha *lista, int tam_lista);
+int encontraColuna(int coluna, Coluna *col_lista, int tam_col);
+Linha *preencheMatriz(int *qtdLinhas);
+
+int main(){
+    Linha *Matriz;
+    int qtd_linhas=0;
+    
+    Matriz = preencheMatriz(&qtd_linhas);
+    printMatriz(Matriz, qtd_linhas);
+
+    for(int i=0;i<qtd_linhas;i++){
+        free(Matriz[i].p);
+    }
+    free(Matriz);
+
+    return 0;
+}
+
+int encontraLinha(int linha, Linha *lista, int tam_lista){
     for(int i=0;i<tam_lista;i++){
-        if((*lista)[i].Linha == linha){
+        if(lista[i].Linha == linha){
             return i;
         }
     }
     return -1;
 }
 
-int encontraColuna(int coluna, Coluna **col_lista, int tam_col){
+int encontraColuna(int coluna, Coluna *col_lista, int tam_col){
     for(int i=0;i<tam_col;i++){
-        if((*col_lista)[i].Coluna == coluna){
+        if(col_lista[i].Coluna == coluna){
             return i;
         }
     }
@@ -34,14 +54,20 @@ Linha *preencheMatriz(int *qtdLinhas){
     int linha, col;
     float valor;
     Linha *Matriz=NULL;
+    Linha *aux;
+    Coluna *aux1;
     printf("Qual é a linha, a coluna e o valor respectivo na matriz? ");
     while(scanf("%d %d %f", &linha, &col, &valor) != EOF){
-        if(encontraLinha(linha, &Matriz, *qtdLinhas) == -1){//não existe a linha ainda logo tbm nn existe coluna
+        if(encontraLinha(linha, Matriz, *qtdLinhas) == -1){//não existe a linha ainda logo tbm nn existe coluna
             int qtd_colunas = 0;
+            aux = Matriz;
             Matriz = (Linha *) realloc(Matriz, (*qtdLinhas + 1) * sizeof(Linha));
+            if(Matriz == NULL){printf("problema de alocacao."); for(int i=0;i<*qtdLinhas;i++) free(aux[i].p); free(aux); exit(1);}
             Matriz[*qtdLinhas].Linha = linha;
             Matriz[*qtdLinhas].qtdCols = qtd_colunas+1;
+            aux1 = Matriz[*qtdLinhas].p;
             Matriz[*qtdLinhas].p = (Coluna *) malloc((qtd_colunas+1) * sizeof(Coluna));
+            if(Matriz[*qtdLinhas].p == NULL){printf("problema de alocacao."); for(int i=0;i<*qtdLinhas;i++) free(Matriz[i].p); free(Matriz); free(aux1); exit(1);}
             Matriz[*qtdLinhas].p[qtd_colunas].Coluna = col;
             Matriz[*qtdLinhas].p[qtd_colunas].Valor = valor;
 
@@ -49,15 +75,17 @@ Linha *preencheMatriz(int *qtdLinhas){
             (*qtdLinhas)++;
         }
         else{// existe linha, não sabe se existe coluna ainda
-            int linha_matriz = encontraLinha(linha, &Matriz, *qtdLinhas);
-            if(encontraColuna(col, &(Matriz[linha_matriz].p), (Matriz[linha_matriz].qtdCols)) == -1){//não achou a coluna, precisa criar ela.
+            int linha_matriz = encontraLinha(linha, Matriz, *qtdLinhas);
+            if(encontraColuna(col, (Matriz[linha_matriz].p), (Matriz[linha_matriz].qtdCols)) == -1){//não achou a coluna, precisa criar ela.
+                aux1 = Matriz[linha_matriz].p;
                 Matriz[linha_matriz].p = (Coluna *) realloc(Matriz[linha_matriz].p, (Matriz[linha_matriz].qtdCols+1)* sizeof(Coluna));
+                if(Matriz[*qtdLinhas].p == NULL){printf("problema de alocacao."); for(int i=0;i<*qtdLinhas;i++) free(Matriz[i].p); free(Matriz); free(aux1); exit(1);}
                 Matriz[linha_matriz].p[Matriz[linha_matriz].qtdCols].Coluna = col;
                 Matriz[linha_matriz].p[Matriz[linha_matriz].qtdCols].Valor = valor;
                 Matriz[linha_matriz].qtdCols += 1;
             }
             else{//achou a coluna e a linha existe
-                int coluna_matriz = encontraColuna(col, &(Matriz[linha_matriz].p), (Matriz[linha_matriz].qtdCols));
+                int coluna_matriz = encontraColuna(col, (Matriz[linha_matriz].p), (Matriz[linha_matriz].qtdCols));
                 Matriz[linha_matriz].p[coluna_matriz].Valor = valor;
             }
         }
@@ -84,10 +112,10 @@ void printMatriz(Linha *matriz, int qtd_linhas){
     printf("\n");
 
     for(int i=1;i<=max_linha;i++){
-        int a = encontraLinha(i, &matriz, qtd_linhas);
+        int a = encontraLinha(i, matriz, qtd_linhas);
         if(a != -1){//encontrei a linha
             for(int j=1;j<=max_colunas;j++){
-                int b = encontraColuna(j, &(matriz[a].p), matriz[a].qtdCols);
+                int b = encontraColuna(j, (matriz[a].p), matriz[a].qtdCols);
                 if(b != -1){//encontrei a coluna
                     printf("%.1f ",matriz[a].p[b].Valor);
                 }
@@ -104,14 +132,4 @@ void printMatriz(Linha *matriz, int qtd_linhas){
         printf("\n");
     }
 
-}
-
-int main(){
-    Linha *Matriz;
-    int qtd_linhas=0;
-    
-    Matriz = preencheMatriz(&qtd_linhas);
-    printMatriz(Matriz, qtd_linhas);
-
-    return 0;
 }
